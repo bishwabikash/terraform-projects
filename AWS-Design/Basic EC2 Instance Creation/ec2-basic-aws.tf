b1c2455647b828189cf6a248edeb2c7dff7ab3cc
Bishwa Bikash Das <bishwa.das94@gmail.com>
2021-06-28 04:33:45 +0530

    Terraform Practical Lab Push

---
+++ AWS-Design/Basic EC2 Instance Creation/ec2-basic-aws.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.27"
+    }
+  }
+  required_version = ">=0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0fcad629e5baa507a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
